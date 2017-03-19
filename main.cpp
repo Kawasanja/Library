02f2449eddaa9da48cdd887011ea0d4d0f324ae9
Alexander Lemeshev <lemeshau@gmail.com>
2017-03-20 00:07:30 +0300

    Build 2

--- main.cpp
+++ main.cpp
@@ -2,39 +2,16 @@
 #include <iterator>
 #include <vector>
 #include <fstream>
+#include <typeinfo>
+#include <algorithm>
 using namespace std;
 
-class Book {
-	private:
+class Vector {
+	public:
 	vector<string> book_vector;
-		
-	public:
-	void set_book_vector (const string & asd){
-		book_vector.push_back (asd);
-		}
-		
-	void get_book_vector (){
-		for (unsigned int i = 0; i<book_vector.size(); i++){
-			cout << book_vector[i] << endl;}
-		}
-		
-};
-/*
-class Name {
-	private:
 	vector<string> name_vector;
-	
-	public:
-	void set_name_vector (vector<string> & asd){
-		name_vector = asd;
-		}
-		
-	void get_name_vector (){
-		for (unsigned int i = 0; i<name_vector.size(); i++){
-			cout << name_vector[i] << endl;}
-		}
-		
-};*/
+};		
+
 
 int main()
 {	
@@ -49,64 +26,139 @@ int main()
 	cout << "8 - return the book from the reader" << endl;
 	cout << "9 - exit" << endl << endl;
 	
-
+	Vector obj;
 	
 do{
 	cout << endl << "Enter the number: ";
 	int a;
 	cin >> a;
-	switch (a)
+	
+	
+switch (a)
 	{
 	case 1: {
+	if(obj.book_vector.size() == 0) {
 		ifstream book_stream;
 		book_stream.open("Books.txt");
 		string b;
-		Book obj;
 		while(getline(book_stream, b)){ 
-			obj.set_book_vector(b);
+			obj.book_vector.push_back(b);
 		} 
 		book_stream.close();
-		obj.get_book_vector();
+		sort(obj.book_vector.begin(),obj.book_vector.end());
+		for (auto i:obj.book_vector){
+			cout << i << endl;}}
+	else {
+		sort(obj.book_vector.begin(),obj.book_vector.end());
+		for (auto i:obj.book_vector){
+			cout << i << endl;}}
 		break;
 	}
-	/*
+	
 	case 2: {
+		if(obj.name_vector.size() == 0) {
 		ifstream name_stream;
 		name_stream.open("Names.txt");
 		string n;
-		Name obj;
 		while(getline(name_stream, n)){ 
 			obj.name_vector.push_back(n);
 		}
 		name_stream.close();
-		obj.get_name_vector();
+		sort(obj.name_vector.begin(),obj.name_vector.end());
+		for (auto i:obj.name_vector){
+			cout << i << endl;}}
+	else {
+		sort(obj.name_vector.begin(),obj.name_vector.end());
+		for (auto i:obj.name_vector){
+			cout << i << endl;}}
 	break;
-	}*/
+	}
+	
 	case 3: {
+		if(obj.book_vector.size() == 0) {
+		ifstream book_stream;
+		book_stream.open("Books.txt");
+		string b;
+		while(getline(book_stream, b)){ 
+			obj.book_vector.push_back(b);
+		} 
+		book_stream.close();
+		sort(obj.book_vector.begin(),obj.book_vector.end());}
 		string asd;
 		cout << "Enter the name of the book and its author: ";
 		getline(cin >> ws, asd);
-		Book obj;
-		obj.set_book_vector(asd);
+		obj.book_vector.push_back(asd);
 		cout << "Done" << endl;
 		break;
 	}
-	case 4: cout << "Th" << endl; break;
-	case 5: cout << "Fr" << endl; break;
-	case 6: cout << "St" << endl; break;
+	
+	case 4: {
+		if(obj.name_vector.size() == 0) {
+		ifstream name_stream;
+		name_stream.open("Names.txt");
+		string n;
+		while(getline(name_stream, n)){ 
+			obj.name_vector.push_back(n);
+		}
+		name_stream.close();
+		sort(obj.name_vector.begin(),obj.name_vector.end());}
+		string asd;
+		cout << "Enter the name of the reader: ";
+		getline(cin >> ws, asd);
+		obj.name_vector.push_back(asd);
+		cout << "Done" << endl;
+		break;
+	}
+	
+	case 5: {
+		if(obj.book_vector.size() == 0) {
+		ifstream book_stream;
+		book_stream.open("Books.txt");
+		string b;
+		while(getline(book_stream, b)){ 
+			obj.book_vector.push_back(b);
+		} 
+		book_stream.close();
+		sort(obj.book_vector.begin(),obj.book_vector.end());}
+		string name;
+		cout << "Enter the name of the book you are looking for: ";
+		getline(cin >> ws, name);
+		auto it = find(obj.book_vector.begin(),obj.book_vector.end(),name);
+		if (it == obj.book_vector.end()) {
+			cout << "No such book" << endl;}
+			else cout << "Such a book exists" << endl;		
+		break;	
+	}
+	
+	case 6: {
+		if(obj.name_vector.size() == 0) {
+		ifstream name_stream;
+		name_stream.open("Names.txt");
+		string n;
+		while(getline(name_stream, n)){ 
+			obj.name_vector.push_back(n);
+		}
+		name_stream.close();
+		sort(obj.name_vector.begin(),obj.name_vector.end());}
+		string name;
+		cout << "Enter the name of the reader you are looking for: ";
+		getline(cin >> ws, name);
+		auto it = find(obj.name_vector.begin(),obj.name_vector.end(),name);
+		if (it == obj.name_vector.end()) {
+			cout << "No such reader" << endl;}
+			else cout << "Such a reader exists" << endl;		
+		break;	
+	}
+	
 	case 7: cout << "Sn" << endl; break;
 	case 8: cout << "Df" << endl; break;
 	case 9: return(0);
 	default: cout << "Incorrect number" << endl; break;
 	}
+	
 }
 while (true);
-	
-	
-
-	
-
-    return 0; 
+return 0; 
 }
 
 
