05fb746bcc6d3935e40288c3521db352e8f451a8
Alexander Lemeshev <lemeshau@gmail.com>
2017-03-24 02:29:17 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <typeinfo>
 #include <algorithm>
+#include <map>
 using namespace std;
 
 
@@ -18,11 +19,13 @@ int main()
 	cout << "6 - find the reader" << endl;
 	cout << "7 - give the book to the reader" << endl;
 	cout << "8 - return the book from the reader" << endl;
-	cout << "9 - exit" << endl << endl;
+	cout << "9 - watch the book's given list" << endl;
+	cout << "10 - exit" << endl;
 	
 	
 	vector<string> book_vector;
 	vector<string> name_vector;
+	map<string,string> bn;
 	
 	if(book_vector.size() == 0) {
 		ifstream book_stream;
@@ -47,11 +50,13 @@ int main()
 	}
 	
 	
-	
+
 do{
-	cout << endl << "Enter the number: ";
+	cout << endl << endl << "Enter the number: ";
 	int a;
 	cin >> a;
+	cin.clear();
+	cin.sync();
 	
 	
 switch (a)
@@ -108,18 +113,58 @@ switch (a)
 		break;	
 	}
 	
-	case 7: cout << "Sn" << endl; break;
-	case 8: cout << "Df" << endl; break;
-	case 9: return(0);
-	default: cout << "Incorrect number" << endl; break;
+	case 7: {
+		cout << endl << "Choose the book: " << endl;
+		sort(book_vector.begin(),book_vector.end());
+		for (unsigned int i = 0; i < book_vector.size(); i++){ cout << i << ' ' << '-' << ' ' << book_vector[i] << endl;}
+	b1:	cout << endl << "Enter the book's code: ";
+		unsigned int b;
+		cin >> b;
+		if (b<0 || b>=book_vector.size()) {cout << "Wrong code" << endl; goto b1;}
+		
+		for (auto i:bn){
+		if (book_vector[b] == i.first) {cout << "The book is already given"; goto b1;}}
+				
+		cout << endl << "Choose the reader: " << endl;
+		sort(name_vector.begin(),name_vector.end());
+		for (unsigned int j = 0; j < name_vector.size(); j++){ cout << j << ' ' << '-' << ' ' << name_vector[j] << endl;}
+	n1:	cout << endl << "Enter the reader's code: ";
+		unsigned int n;
+		cin >> n;
+		if (n<0 || n>=name_vector.size()) {cout << "Wrong code" << endl; goto n1;}
+				
+		bn.insert (make_pair(book_vector[b], name_vector[n]));
+		cout << endl << "The book " << book_vector[b] << " is given to " << name_vector[n];
+		break;
+	}
+	
+	case 8: {
+		int j = 0;
+		for (auto i:bn) {
+			cout << endl << j << ' ' << '-' << ' ' << "The book " << i.first << " is given to " << i.second;
+			j++;}
+			cout << endl << endl << "Choose the code to delete: ";
+			unsigned int b;
+			cin >> b;
+			
+			for (auto i = bn.begin(); i != bn.end(); i++){
+			bn.erase(book_vector[b]);}
+			cout << "Delete complete";
+			
+		break;
+	}
+	
+	case 9: {
+		for (auto i:bn) {cout << endl << "The book " << i.first << " is given to " << i.second;}
+		break;
+	}
+	
+	case 10: return(0);
+	
+	default: {cout << "Incorrect enter" << endl; break;}
 	}
 	
 }
 while (true);
 return 0; 
 }
-
-
-
-
-
